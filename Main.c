12386f6f349d0a5e744b86153c10762c1a37ed7f
fungikami <kafung09@gmail.com>
2022-06-16 19:35:13 -0400

    Eliminar \n

--- Main.c
+++ Main.c
@@ -38,9 +38,7 @@ int main(int argc, char **argv) {
         
         if (i != argc - 1) {
             printf("\n--\n");
-        } else {
-            printf("\n");
-        }
+        } 
     }
 
     /* Liberar memoria ocupada por la lista */
